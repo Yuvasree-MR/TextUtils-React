884ece1946f24babff4b6c19e9efc2793c19771c
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-11 19:58:50 +0530

    Created React-Router-Dom to navigate About and TextForm

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  // Switch,
+  Route,
+  // Link
+} from "react-router-dom";
 
 
 function App() {
@@ -27,6 +34,12 @@ function App() {
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode has been Enabled", "success");
       document.title = "TextUtils - Dark Mode";
+      // setInterval(() => {
+      //   document.title = "TextUtils is Amazing";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils";
+      // }, 1500);
     }
     else{
       setMode('light')
@@ -39,25 +52,32 @@ function App() {
 
   return (
     <>
-    {/* <Navbar title="Text Utils" aboutText="About TextUtils"/> */}
-    {/* for default props */}
-    {/* <Navbar/> */}
+      <Router>
+      
+        {/* <Navbar title="Text Utils" aboutText="About TextUtils"/> */}
+        {/* for default props */}
+        {/* <Navbar/> */}
 
-    <Navbar title="TextUtils" mode = {mode} toggleMode={toggleMode}/>
+        <Navbar title="TextUtils" mode = {mode} toggleMode={toggleMode}/>
 
-    <Alert alert={alert}/>
+        <Alert alert={alert}/>
 
-    {/* using JS default parameters instead of defaultProps */}
-    {/* <Navbar title='Text Utils' aboutText='About Us'/> */}
+        {/* using JS default parameters instead of defaultProps */}
+        {/* <Navbar title='Text Utils' aboutText='About Us'/> */}
 
-    <div className="container my-3">
-      <TextForm showAlert={showAlert} heading="Enter the Text to Analyze below" mode={mode}/>
-
-      {/* <About/> */}
-    </div>
-    
-
+          <div className="container my-3">
+            <Routes>
+              <Route exact path="/about" element = { <About/> }>
+              </Route>
+              
+              <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try Text Utils - Word Counter - Character Counter, Remove Extra Spaces" mode={mode}/>}> 
+              </Route>
+            </Routes>
+          </div>
+        
+      </Router>
     </>
+    
   );
 }
 
