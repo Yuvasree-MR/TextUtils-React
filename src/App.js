edd284761dfd5a79c71a0f74525fdab076705bc1
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-12 13:10:52 +0530

    Added Different Toggle Modes

--- src/App.js
+++ src/App.js
@@ -28,9 +28,26 @@ function App() {
     }, 1500)
   }
 
-  const toggleMode = () => {
+  // to remove the previous togglemode colors
+  // const removeBodyClasses = () => {
+  //   document.body.classList.remove('bg-light')
+  //   document.body.classList.remove('bg-dark')
+  //   document.body.classList.remove('bg-warning')
+  //   document.body.classList.remove('bg-success')
+  //   document.body.classList.remove('bg-danger')
+  // }
+
+  // to add different togglemodes
+  // const toggleMode = (cls) => {
+  //   removeBodyClasses();
+  //   console.log(cls);
+
+    const toggleMode = () => {
+
+    // to add different togglemodes
+    // document.body.classList.add('bg-'+cls)
     if(mode === 'light'){
-      setMode('dark')
+      setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode has been Enabled", "success");
       // document.title = "TextUtils - Dark Mode";
@@ -42,9 +59,9 @@ function App() {
       // }, 1500);
     }
     else{
-      setMode('light')
+      setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light Mode has been Enabled", "succes");
+      showAlert("Light Mode has been Enabled", "success");
       // document.title = "TextUtils - Light Mode";
     }
     
@@ -58,7 +75,7 @@ function App() {
         {/* for default props */}
         {/* <Navbar/> */}
 
-        <Navbar title="TextUtils" mode = {mode} toggleMode={toggleMode}/>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
 
         <Alert alert={alert}/>
 
