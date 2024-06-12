36a1dfcc13f5da6e7bd053e3ba1cba6339868a85
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-12 10:36:31 +0530

    Fixed Button overlays and word count

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -49,16 +49,16 @@ export default function TextForm(props) {
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : '#042743'}} id="myBox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-      <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
-      <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-      <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-      <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to LowerCase</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
     </div>
 
     <div className='container my-3' style={{color: props.mode === 'dark' ? 'white' : '#042743'}}>
       <h2>Your text summary</h2>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{text.split(" ").filter((element) => {return element.length !== 0}).length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} Minutes read</p>
       <h1>Preview</h1>
       <p>{text.length>0?text:"Enter Something to Preview it here"}</p>
