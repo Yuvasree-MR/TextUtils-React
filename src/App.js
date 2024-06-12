08cf028604b12d345d5d9051bb6fde62941d7365
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-12 15:30:43 +0530

    Deployed in Github using ghpages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  // Switch,
-  Route,
-  // Link
-} from "react-router-dom";
+
+
+// Removing Route for gh-pages
+
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   // Switch,
+//   Route,
+//   // Link
+// } from "react-router-dom";
 
 
 function App() {
@@ -69,30 +73,48 @@ function App() {
 
   return (
     <>
-      <Router>
-      
+
+
+      {/* <Router> */}
+
+
+{/* prev1 */}
         {/* <Navbar title="Text Utils" aboutText="About TextUtils"/> */}
         {/* for default props */}
         {/* <Navbar/> */}
+{/* prev1 */}
 
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
 
-        <Alert alert={alert}/>
+        {/* <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/> */}
 
+        {/* <Alert alert={alert}/> */}
+
+{/* prev2 */}
         {/* using JS default parameters instead of defaultProps */}
         {/* <Navbar title='Text Utils' aboutText='About Us'/> */}
+{/* prev2 */}
 
-          <div className="container my-3">
-            <Routes>
-              <Route exact path="/about" element = {<About mode={mode}/>}>
-              </Route>
+          {/* <div className="container my-3"> */}
+            {/* <Routes> */}
+              {/* <Route exact path="/about" element = {<About mode={mode}/>}> */}
+              {/* </Route> */}
               
-              <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try Text Utils - Word Counter - Character Counter, Remove Extra Spaces" mode={mode}/>}> 
-              </Route>
-            </Routes>
-          </div>
-        
-      </Router>
+              {/* <Route exact path="/" element={ */}
+                {/* <TextForm showAlert={showAlert} heading="Try Text Utils - Word Counter - Character Counter, Remove Extra Spaces" mode={mode}/> */}
+                {/* }>  */}
+              {/* </Route> */}
+            {/* </Routes> */}
+          {/* </div> */}
+      {/* </Router> */}
+
+
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+
+      <Alert alert={alert}/>
+      
+      <div className="container my-3">
+        <TextForm showAlert={showAlert} heading="Try Text Utils - Word Counter - Character Counter, Remove Extra Spaces" mode={mode}/>
+      </div>
     </>
     
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -47,7 +47,7 @@ export default function TextForm(props) {
   return (
     <>
     <div className='container' style={{color: props.mode === 'dark' ? 'white' : '#042743'}}>
-      <h1  className='mb-4'>{props.heading}</h1>
+      <h2  className='mb-4'>{props.heading}</h2>
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? '#13466e' : 'white', color: props.mode === 'dark' ? 'white' : '#042743'}} id="myBox" rows="8"></textarea>
       </div>
