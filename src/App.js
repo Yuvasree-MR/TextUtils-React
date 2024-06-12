f5c4878ae9de34ca277544c91dec04a6614ef8ce
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-12 11:44:02 +0530

    Altered some tetx utils issues

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark Mode has been Enabled", "success");
-      document.title = "TextUtils - Dark Mode";
+      // document.title = "TextUtils - Dark Mode";
       // setInterval(() => {
       //   document.title = "TextUtils is Amazing";
       // }, 2000);
@@ -45,7 +45,7 @@ function App() {
       setMode('light')
       document.body.style.backgroundColor = 'white';
       showAlert("Light Mode has been Enabled", "succes");
-      document.title = "TextUtils - Light Mode";
+      // document.title = "TextUtils - Light Mode";
     }
     
   }
@@ -67,7 +67,7 @@ function App() {
 
           <div className="container my-3">
             <Routes>
-              <Route exact path="/about" element = { <About/> }>
+              <Route exact path="/about" element = {<About mode={mode}/>}>
               </Route>
               
               <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try Text Utils - Word Counter - Character Counter, Remove Extra Spaces" mode={mode}/>}> 
